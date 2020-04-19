42743b79645f9e68dad2fc7a4b745274546824ba
Jackson Vanover <jacksonvanover@gmail.com>
2020-04-19 12:45:18 -0700

    added separate iterative testing and single testing methods for throw_the_lever

--- src/throw_the_lever.py
+++ src/throw_the_lever.py
@@ -3,14 +3,55 @@
 from spectral_clustering_solution import ClusteringRecommender
 
 
-print("\n====== INITIALIZING RECOMMENDER ======\n")
-recommender = ClusteringRecommender(production=False)
+def test():
+    print("\n====== INITIALIZING RECOMMENDER ======\n")
+    recommender = ClusteringRecommender(production=False)
 
-print("\n====== PERFORMING CLUSTERING ======\n")
-recommender.cluster(n_clusters=25, n_components=7, n_iter=10, n_neighbors=500)
+    clusters = [5,6,7,8]
+    components = [3, 4, 5, 6]
+    neighbors = [250, 500, 1000]
 
-print("\n====== MAKING RECOMMENDATIONS ======\n")
-recommender.recommend()
+    configs = {}
+    counter = 1
+    for x in clusters:
+        for y in components:
+            for z in neighbors:
+                
+                print("=========")
+                print("Testing config {}/60".format(counter))
+                print("{} clusters, {} eigenvectors, {} nearest neighbors".format(x,y,z))
 
-print(recommender.calculate_error())
-recommender.print_stats_for_nerds()
+                recommender.cluster(n_clusters=x, n_components=y, n_iter=10, n_neighbors=z)
+                recommender.recommend()
+
+                error = recommender.calculate_error()
+                print("\tMSE: {}\n".format(error))
+
+                configs["{},{},{}".format(x,y,z)] = error
+                counter += 1
+
+    minimum = ("",10000)
+    with open("output.txt", "w") as f:
+        for config, error in configs.items():
+            if error < minimum[1]:
+                minimum = (config, error)
+            print("{:>15} => {}".format(config, error))
+
+    print(minimum)
+
+def main():
+    print("\n====== INITIALIZING RECOMMENDER ======\n")
+    recommender = ClusteringRecommender(production=False)
+
+    recommender.cluster(n_clusters=5, n_components=3, n_iter=10, n_neighbors=1000)
+    recommender.recommend()
+
+    error = recommender.calculate_error()
+    print("\tMSE: {}\n".format(error))
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "test":
+        test()
+    else:
+        main()
