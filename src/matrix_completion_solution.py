ee69462b9fd085a2583734830b0750266ea536d2
jacksonvanover <jacksonvanover@gmail.com>
2020-04-21 11:50:05 -0700

    working matrix completion solution

--- src/matrix_completion_solution.py
+++ src/matrix_completion_solution.py
@@ -1,47 +1,148 @@
-import numpy as np
-import pandas as pd
 import os
+import pickle
+import pandas as pd
+import numpy as np
+
 from scipy.sparse import csc_matrix
-from scipy.linalg import svd
+from scipy.linalg import svd, sqrtm
+
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_squared_error
+
 from split_training_set import split_training_set
+
+from pprint import pprint
 
 # set CWD to root of project directory tree
 os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
-split_training_set()
 
-# load dataset
-df_filled = pd.read_csv("./data/my_train.csv")
-df_blank = pd.read_csv("./data/my_test_blank.csv")
+class MatrixCompletionRecommender():
+    
+    def __init__(self, production=True, splitter="my-splitter"):
 
-# convert all question marks to zeros
-df_blank["rating"] = df_blank["rating"].apply(lambda x : 0)
+        self.production = production
+        self.splitter = splitter
 
-# merge the two
-df = pd.concat([df_filled,df_blank], ignore_index=True, sort=False).sort_values(by=['customer-id', 'movie-id']).reset_index(drop=True)
+        if self.production:
+            self.df_filled = pd.read_csv("./data/train.csv")
+            self.df_blank = pd.read_csv("./data/test.csv")
 
-# dicts for mapping ids to indices in the customer_nodes matrix
-cid_to_index = {key:value for value, key in enumerate(df["customer-id"].unique())}
-mid_to_index = {key:value for value, key in enumerate(df["movie-id"].unique())}
+        else:
+            print("\n====== SPLITTING TRAINING SET ======\n")
 
-# construct customer_nodes matrix
-row = np.array(df['customer-id'].apply(lambda x : cid_to_index[x]).tolist())
-col = np.array(df['movie-id'].apply(lambda x : mid_to_index[x]).tolist())
-data = np.array(df['rating'].tolist())
-customer_nodes = csc_matrix( (data,(row,col)), shape=(len(cid_to_index), len(mid_to_index))).todense()
+            if splitter == "my-splitter":
+                split_training_set()
+            
+                self.df_filled = pd.read_csv("./data/my_train.csv")
+                self.df_blank = pd.read_csv("./data/my_test_blank.csv")
+                self.df_answers = pd.read_csv("./data/my_test_answers.csv")
 
-my_imputer = SimpleImputer(missing_values=0, strategy='mean')
-my_imputer.fit_transform(customer_nodes)
+            elif splitter == "sklearn":
+                split = train_test_split(pd.read_csv("./data/train.csv"))
 
-U, s, V = svd(customer_nodes)
+                self.df_filled = split[0]
+                self.df_answers = split[1]
+                self.df_blank = self.df_answers.copy(deep=True)
+                self.df_blank["rating"] = self.df_blank["rating"].apply(lambda x : "?")
 
-import pickle
-with open("U.pckl", "wb") as f:
-    pickle.dump(U, f)
+            elif splitter == "preprocessed":
+                self.df_filled = pd.read_csv("./data/my_train.csv")
+                self.df_blank = pd.read_csv("./data/my_test_blank.csv")
+                self.df_answers = pd.read_csv("./data/my_test_answers.csv")                
 
-with open("V.pckl", "wb") as f:
-    pickle.dump(V, f)
+            else:
+                raise Exception("Must specify a splitting method")
+    
+        # convert all question marks to zeros
+        self.df_blank["rating"] = self.df_blank["rating"].apply(lambda x : 0)
 
-with open("s.pckl", "wb") as f:
-    pickle.dump(s, f)
+        # merge the two
+        self.df_combined = pd.concat([self.df_filled,self.df_blank], ignore_index=True, sort=False).sort_values(by=['customer-id', 'movie-id']).reset_index(drop=True)
+
+        # dicts for mapping ids to indices in the customer_nodes matrix
+        self.cid_to_index = {key:value for value, key in enumerate(self.df_combined["customer-id"].unique())}
+        self.mid_to_index = {key:value for value, key in enumerate(self.df_combined["movie-id"].unique())}
+
+        self.customer_nodes = self.build_customer_nodes_matrix()
+
+
+    def build_customer_nodes_matrix(self):
+
+        row = np.array(self.df_combined['customer-id'].apply(lambda x : self.cid_to_index[x]).tolist())
+        col = np.array(self.df_combined['movie-id'].apply(lambda x : self.mid_to_index[x]).tolist())
+        data = np.array(self.df_combined['rating'].tolist())
+        customer_nodes = csc_matrix( (data,(row,col)), shape=(len(self.cid_to_index), len(self.mid_to_index))).todense()
+
+        return customer_nodes
+
+
+    def impute(self, matrix):
+        matrix = np.where(matrix==0, np.nan, matrix)
+
+        my_imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+        return my_imputer.fit_transform(matrix)
+
+
+    def cluster(self, k=5):
+
+        imputed_customer_nodes = self.impute(self.customer_nodes)
+
+        if self.splitter == "preprocessed" and os.path.exists("./data/V.pckl"):
+            with open("./data/U.pckl", "rb") as f:
+                U = pickle.load(f)
+
+            with open("./data/V.pckl", "rb") as f:
+                V = pickle.load(f)
+
+            with open("./data/s.pckl", "rb") as f:
+                s = pickle.load(f)
+
+        else:
+            U, s, V = np.linalg.svd(imputed_customer_nodes, full_matrices=False)
+
+            with open("./data/U.pckl", "wb") as f:
+                pickle.dump(U, f)
+
+            with open("./data/V.pckl", "wb") as f:
+                pickle.dump(V, f)
+
+            with open("./data/s.pckl", "wb") as f:
+                pickle.dump(s, f)
+
+        s = sqrtm(np.diag(s)[0:k, 0:k])
+        U=U[:, 0:k]
+        V=V[0:k, :]
+
+        Usk=np.dot(U,s)
+        skV=np.dot(s,V)
+        UsV = np.dot(Usk, skV)
+
+        self.recommender_matrix = UsV
+
+
+    def estimate_rating(self, cid, mid):
+
+        temp = round(self.recommender_matrix[self.cid_to_index[cid]][self.mid_to_index[mid]])
+
+        return temp
+
+
+    def recommend(self):
+        # fill in estimated ratings
+        self.df_blank["rating"] = self.df_blank.apply(lambda row : self.estimate_rating(row["customer-id"], row["movie-id"]), axis=1)
+        
+        # write out result
+        with open("jacksonvanover_preds_matrix.txt", "w") as f:
+            for rating in self.df_blank["rating"].tolist():
+                print(int(rating), file=f)
+
+        self.df_blank.to_csv("./data/my_test_filled.csv", index=False)
+
+
+    def calculate_error(self):
+        if self.production:
+            raise Exception("No ground truth to evaluate against!")
+        else:
+            return mean_squared_error(self.df_answers["rating"], self.df_blank["rating"])
+            
