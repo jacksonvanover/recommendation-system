7d5a1fdb1b8714e0caff47b048e30348663c357e
jacksonvanover <jacksonvanover@gmail.com>
2020-04-23 17:03:11 -0700

    switched driver to production mode

--- src/throw_the_lever.py
+++ src/throw_the_lever.py
@@ -5,8 +5,8 @@ from matrix_completion_solution import MatrixCompletionRecommender
 
 def main():
     print("\n====== INITIALIZING RECOMMENDERS ======\n")
-    recommender1 = MatrixCompletionRecommender(production=False, splitter="my-splitter")
-    recommender2 = ClusteringRecommender(production=False, splitter="preprocessed")
+    recommender1 = MatrixCompletionRecommender(production=True)
+    recommender2 = ClusteringRecommender(production=True)
 
     print("\n====== PERFORMING CLUSTERING ======\n")
     recommender1.cluster()
